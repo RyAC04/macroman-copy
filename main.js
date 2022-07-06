9f289245632862ac0b6ab28225c716d565af7d61
RyAC04 <ryanac04@gmail.com>
2022-07-06 15:14:20 +0800

    first commit sp-767

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	$(window).scroll(function () {
-		if($(this).scrollTop() > 40){
-			$('#to-top').fadeIn();
-		} else {
-			$('#to-top').fadeOut();
-		}
-	});
+// 	$(window).scroll(function () {
+// 		if($(this).scrollTop() > 40){
+// 			$('#to-top').fadeIn();
+// 		} else {
+// 			$('#to-top').fadeOut();
+// 		}
+// 	});
 
 
-	$('#to-top').click(function(){
-		$('html, body').animate({scrollTop : 0,},600);
-	});
-});
+// 	$('#to-top').click(function(){
+// 		$('html, body').animate({scrollTop : 0,},600);
+// 	});
+// });
+
+
+// const toggleButton =document.getElementsByClassName()
+
+let toggleButton = document.getElementsByClassName('toggle-button')[0];
+let navbarLinks = document.getElementsByClassName('header__nav')[0];
+
+// toggleButton.addEventListener('click', btnClick)
+// 	// navbarLinks.classList.toggle('active')
+
+// )
+
+toggleButton.addEventListener('click', btnClick);
+
+function btnClick(){
+	console.log('this click');
+	navbarLinks.classList.toggle('active');
+
+
+	toggleButton.classList.toggle('drop');
+
+};
+console.log(toggleButton)
+console.log(navbarLinks)
